6b5450ec2a1822fdf31c94310156dc07a7c23abf
ushion0a <1.next632@passfwd.com>
2026-09-05 11:46:11 +0800

    config: allow device-specific Axion configs

--- config/board/common.mk
+++ config/board/common.mk
@@ -2,9 +2,12 @@ TARGET_SHIPS_AXION_KERNEL_MODULES := true
 TARGET_SUPPORTS_KERNEL_MANAGER := true
 TARGET_DISABLES_LIBPERF := true
 
+AXION_KERNEL_MANAGER_CONFIG ?= device/axion/common/prebuilts/ax_kernel_manager_$(AXION_SOC).xml
+AXION_PERF_THERMAL_CONFIG ?= device/axion/common/prebuilts/ax_perf_thermal_$(AXION_SOC).xml
+
 ifeq ($(TARGET_SUPPORTS_KERNEL_MANAGER),true)
 PRODUCT_COPY_FILES += \
-    device/axion/common/prebuilts/ax_kernel_manager_$(AXION_SOC).xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/ax_kernel_manager.xml
+    $(AXION_KERNEL_MANAGER_CONFIG):$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/ax_kernel_manager.xml
 endif
 
 TARGET_PRODUCT_PROP += device/axion/common/platform/$(AXION_SOC)/props/ax_$(AXION_SOC).prop
@@ -13,4 +16,4 @@ PRODUCT_COPY_FILES += \
     device/axion/common/init/init.axion.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.axion-common.rc \
     device/axion/common/init/init.axion.modules.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.axion.modules.rc \
     device/axion/common/init/ax_init_$(AXION_SOC).rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/ax_init_$(AXION_SOC).rc \
-    device/axion/common/prebuilts/ax_perf_thermal_$(AXION_SOC).xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/ax_perf_thermal.xml
+    $(AXION_PERF_THERMAL_CONFIG):$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/ax_perf_thermal.xml
